287fe8f40a406d3dbafd035c12ef294018559b83
user-default <71413474+user-default@users.noreply.github.com>
2020-10-07 16:48:19 -0300

    Q5.java
    Utilizando a estrutura de repetição while, faça um programa que escreva todos os números entre 0 e 100.

---
+++ Q5.java
@@ -0,0 +1,13 @@
+public class Main
+{
+  public static void main(String [] args)
+  { 
+    int i = 0;
+    while(i < 100)
+    {
+      i++;
+      System.out.printf("%d\t", i);
+      
+    } 
+  }
+} 
